7c56b8ded27c1c072d93046b59cd98de30cb24d8
ElenaR <llinusik@mail.ru>
2023-10-31 16:22:11 +0300

    1.1

--- core/src/com/mygdx/game/Background.java
+++ core/src/com/mygdx/game/Background.java
@@ -5,22 +5,43 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Background {
-    private Texture tx; //картинка фона
-    private Vector2 pos; //создание позиции вектор 2 координаты
-    private int speed; //скорость отрисовки
 
+    class BGPicture{
+        private Texture tx; //картинка фона
+        private Vector2 pos; //создание позиции вектор 2 координаты
+
+
+        public BGPicture(Vector2 pos){ // данный конструктор инициализирует координаты
+            tx = new Texture("back.png");
+            this.pos = pos;
+        }
+    }
+    private int speed; //скорость отрисовки
+    private BGPicture[] backs;
 
     public Background (){
-        tx = new Texture("back.png");
-        pos = new Vector2(0,0);
+
         speed = 4; //на 4 точки во время перерисовки
+        backs = new BGPicture[2];
+        backs[0] = new BGPicture(new Vector2(0,0));
+        backs[1] = new BGPicture(new Vector2(800,0));
     }
 
     public void render (SpriteBatch batch){ //отрисовка, параметр с помощью чего
-        batch.draw (tx, pos.x, pos.y);
+        for (int i = 0; i < backs.length; i++) {
+            batch.draw(backs[i].tx, backs[i].pos.x, backs[i].pos.y);
+            
+        }
     }
 
     public void update(){
-        pos.x -= speed;
+        for (int i = 0; i < backs.length; i++) {
+            backs[i].pos.x -= speed;
+                    }
+
+        if (backs[0].pos.x < -800){ //делаем фон подвижным непрерывно
+            backs[0].pos.x = 0;
+            backs[1].pos.x = 800;
+        }
     }
 }
